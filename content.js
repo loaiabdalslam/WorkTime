9fc0b3bf4cdccad93113922f0a41ff1aba6028b8
loaiabdalslam <loaiabdalslam@gmail.com>
2019-04-25 14:41:30 +0200

    change extenstion and make it for display whole newfeed at facebook

--- content.js
+++ content.js
@@ -5,52 +5,16 @@ const state = {
   postPath: '',
   postsLength: 0,
   wordsLength: 0,
+  posts:document.getElementById('stream_pagelet').style.display,
 };
 
-// here let's make it more dynamic with facebook and twitter and linkedin
 
-if (state.page.includes('facebook.com')) {
-  state.postsPath = document.getElementsByClassName('_5jmm');
-} else if (state.page.includes('twitter.com')) {
-  state.postsPath = document.getElementsByClassName('tweet');
-}
+// we using set Interval to force react onpage load and refresh 
+let posts = setInterval(()=>{
+if(document.getElementById('stream_pagelet').style.display !== 'none' ){
+    // remove notification and posts stream it's so sample ..
+    document.getElementById('stream_pagelet').style.display='none';
+    document.getElementsByClassName('_5r-_ homeWiderContent homeFixedLayout newsFeedComposer _1qkq _1ql0')[0].style.display='none'
+  } 
+},5000)
 
-const methodsCases = {
-  remove() {
-    // here we get all posts from facebook
-    const allPosts = Array.from(state.postsPath);
-    window.chrome.storage.sync.get(['words'], (res) => {
-      // we add ourword in storage google just get it please and change it to json
-      const currentWords = JSON.stringify(res) !== '{}' ? JSON.parse(res.words) : [];
-      // okay just equal every thing with state props
-      if (allPosts.length === state.postsLength && currentWords.length === state.wordsLength) return;
-      state.postsLength = allPosts.length;
-      state.wordsLength = currentWords.length;
-      // For each posts in new feeds Foreach word in post:
-      // if post style == display none dosen't make any thing
-      // if word in somewhere has word in blocked words just make it none
-      allPosts.forEach((post) => {
-        currentWords.forEach((word) => {
-          if (post.style.display === 'none') return;
-          if (post.textContent.toLowerCase().includes(word.toLowerCase())) {
-            post.style.display = 'none';
-            return undefined; // critical return
-          }
-        });
-      });
-    });
-  },
-};
-
-window.chrome.runtime.onMessage.addListener(({
-  method,
-  data
-}) => {
-  methodsCases[method](data);
-});
-
-const observer = new MutationObserver(methodsCases.remove);
-observer.observe(document, {
-  childList: true,
-  subtree: true
-});
